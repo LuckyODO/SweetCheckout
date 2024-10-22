a9bb302024f7ba57d519017d8e7623f869f4107e
人間工作 <coolxiaom95@gmail.com>
2024-10-22 23:18:47 +0800

    add orders operate

--- backend/src/main/java/top/mrxiaom/sweet/checkout/backend/data/ClientInfo.java
+++ backend/src/main/java/top/mrxiaom/sweet/checkout/backend/data/ClientInfo.java
@@ -1,8 +1,8 @@
 package top.mrxiaom.sweet.checkout.backend.data;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
 
 public class ClientInfo {
     public static class Order {
@@ -12,12 +12,32 @@ public class ClientInfo {
         String money;
     }
     private final Map<String, Order> orders = new HashMap<>();
+    private final Set<String> locked = new HashSet<>();
 
     public String nextOrderId() {
         String id;
         do {
-            id = UUID.randomUUID().toString();
-        } while (orders.containsKey(id));
+            id = UUID.randomUUID().toString().replace("-", "");
+        } while (orders.containsKey(id) || locked.contains(id));
+        locked.add(id);
         return id;
+    }
+
+    public boolean addOrder(Order order) {
+        if (orders.containsKey(order.id)) return false;
+        orders.put(order.id, order);
+        locked.remove(order.id);
+        return true;
+    }
+
+    @Nullable
+    public Order getOrder(String orderId) {
+        return orders.get(orderId);
+    }
+
+    @Nullable
+    public Order removeOrder(Order order) {
+        locked.remove(order.id);
+        return orders.remove(order.id);
     }
 }
