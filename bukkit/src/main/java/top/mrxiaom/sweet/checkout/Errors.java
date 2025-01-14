5ae36bb145f1fd19768464adae7b8fe775fe4277
人間工作 <coolxiaom95@gmail.com>
2025-01-15 00:07:07 +0800

    add maybe all errors into Errors enum

--- bukkit/src/main/java/top/mrxiaom/sweet/checkout/Errors.java
+++ bukkit/src/main/java/top/mrxiaom/sweet/checkout/Errors.java
@@ -10,8 +10,14 @@ import static top.mrxiaom.pluginbase.func.language.LanguageEnumAutoHolder.wrap;
 
 @Language(prefix = "errors.")
 public enum Errors implements IHolderAccessor {
-    // TODO: 来自后端的错误提示
-    unknown("&e未知错误 %type%")
+    unknown("&e未知错误 %type%"),
+    payment__not_a_number("&e请输入正确的金额"),
+    payment__already_requested("&e请先完成正在进行的订单"),
+    payment__type_unknown("&e未知支付类型"),
+    payment__cancel__not_found("&e你要取消的订单不存在"),
+    payment__hook_price_locked("&e支付系统繁忙，请稍后再试"),
+    payment__can_not_create_id("&e支付系统生成订单号繁忙，请稍后再试"),
+    payment__internal_error("&e支付接口出现内部错误，请联系服务器管理员"),
 
     ;
     Errors(String defaultValue) {
