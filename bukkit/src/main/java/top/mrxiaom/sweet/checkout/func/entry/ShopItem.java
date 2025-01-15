3c226e79c7e717758cd840a236118193a9950bd0
人間工作 <coolxiaom95@gmail.com>
2025-01-15 16:40:06 +0800

    permission

--- bukkit/src/main/java/top/mrxiaom/sweet/checkout/func/entry/ShopItem.java
+++ bukkit/src/main/java/top/mrxiaom/sweet/checkout/func/entry/ShopItem.java
@@ -9,14 +9,16 @@ import java.util.List;
 public class ShopItem {
     public final String id;
     public final String display;
+    public final @Nullable String permission;
     public final List<String> names;
     public final boolean paymentAlipay, paymentWeChat;
     public final String price;
     public final List<String> rewards;
 
-    public ShopItem(String id, String display, List<String> names, boolean paymentAlipay, boolean paymentWeChat, String price, List<String> rewards) {
+    public ShopItem(String id, String display, @Nullable String permission, List<String> names, boolean paymentAlipay, boolean paymentWeChat, String price, List<String> rewards) {
         this.id = id;
         this.display = display;
+        this.permission = permission;
         this.names = names;
         this.paymentAlipay = paymentAlipay;
         this.paymentWeChat = paymentWeChat;
@@ -44,6 +46,8 @@ public class ShopItem {
             plugin.getLogger().warning("[shops] 加载 " + id + " 失败: 奖励命令列表为空");
             return null;
         }
-        return new ShopItem(id, display, names, paymentAlipay, paymentWeChat, String.format("%.2f", price), rewards);
+        String permission = config.getString("permission", null);
+        return new ShopItem(id, display, permission == null ? null : permission.replace("%id%", id),
+                names, paymentAlipay, paymentWeChat, String.format("%.2f", price), rewards);
     }
 }
