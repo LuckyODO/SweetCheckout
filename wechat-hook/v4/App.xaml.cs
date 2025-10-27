304b10d9a311dd71345cd7d682ec11114a6b1fef
MrXiaoM <coolxiaom95@gmail.com>
2025-10-27 17:49:19 +0800

    write unhandled exceptions to file

--- wechat-hook/v4/App.xaml.cs
+++ wechat-hook/v4/App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Text;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace WeChatHook
 {
@@ -13,8 +15,18 @@ namespace WeChatHook
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            DispatcherUnhandledException += (sender, e) => HandleException(e.Exception);
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) => HandleException((Exception) e.ExceptionObject);
+            TaskScheduler.UnobservedTaskException += (sender, e) => HandleException(e.Exception);
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         }
+
+        private void HandleException(Exception ex)
+        {
+            var encoding = new UTF8Encoding(false);
+            File.WriteAllText("LastError.log", ex.ToString(), encoding);
+        }
+
     }
 
 }
