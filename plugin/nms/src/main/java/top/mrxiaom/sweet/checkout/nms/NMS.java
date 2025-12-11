017fcd40c134aaf13a40de1338d3637ebe08d8a8
MrXiaoM <coolxiaom95@gmail.com>
2025-12-12 01:06:25 +0800

    nothing changes in `v1_21_R7` of nbt api

--- plugin/nms/src/main/java/top/mrxiaom/sweet/checkout/nms/NMS.java
+++ plugin/nms/src/main/java/top/mrxiaom/sweet/checkout/nms/NMS.java
@@ -18,7 +18,7 @@ public class NMS {
     public static boolean init() {
         if (loaded) return true;
         MinecraftVersion version = MinecraftVersion.getVersion();
-        if (version.equals(MC1_21_R6)) {
+        if (version.equals(MC1_21_R7) || version.equals(MC1_21_R6)) {
             mapPacket = new MapPacket_v1_21_R6();
             return loaded = true;
         }
